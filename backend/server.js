9e4be2d602401a44aebca866848c2c068220688e
AshitHere <ashitkumar023@gmail.com>
2026-05-25 21:05:12 +0530

    Fixed frontend import casing

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://eatmeta.vercel.app",
+  credentials: true
+}));
 // routes
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
